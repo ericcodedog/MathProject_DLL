90b02d51aaa18209108f92e426abf5ce058ce302
ericcodedog <eric7seven@gmail.com>
2025-05-18 07:05:43 +0800

    Initial commit

---
+++ MathLibrary/MathOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace MathLibrary
+{
+    public class MathOperations
+    {
+        private static readonly string logFilePath = "log.txt";
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                LogError("Cannot divide by zero.");
+                return double.NaN;
+            }
+            return (double)a / b;
+        }
+        private void LogError(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+    }
+}
---
+++ MathLibraryTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MathLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        MathOperations math = new MathOperations();
+        Console.WriteLine("5 + 3 = " + math.Add(5, 3));
+        Console.WriteLine("10 - 7 = " + math.Subtract(10, 7));
+        Console.WriteLine("20 / 4 = " + math.Divide(20, 4));
+        Console.WriteLine("20 * 4 = " + math.Multiply(20, 4));
+        Console.WriteLine("8 / 0 = " + math.Divide(8, 0));
+    }
+}
